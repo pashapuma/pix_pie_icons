711881e59becd38566beee61e938e7638fdecde7
pashapuma <pashapuma1@gmail.com>
2023-07-28 14:16:26 +0300

    Update walls.js

--- walls.js
+++ walls.js
@@ -1,15 +1,3 @@
-$(document).ready(function() {
-
-    function displayPhotos(data) {
-      var photoHTML = "";
-      $.each(data,function(i,photo) {
-          photoHTML += '<div class="picbox"><figure><img src="' + photo.url + '" class="frame" onclick="document.location=this.src"><figcaption>' + photo.description + '</figcaption></figure></div>';
-          
-      }); // end each
-      
-      $('#photos').html(photoHTML);
-    };
-
-    $.getJSON("pix_wall.json", displayPhotos);
-    
-}); // end ready
+{
+  "valid": true
+}
